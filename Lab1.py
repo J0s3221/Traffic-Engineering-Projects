f8015a4f78838290ae73381e2a69f508d1ceb760
J0s3221 <athydeforking@gmail.com>
2026-04-20 18:01:45 +0100

    Feat: added step 1 of 2.2
    Co-authored-by: Tiago Videira <tiagovideira8@users.noreply.github.com>

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,41 @@
 #Etraf first project -- Poisson Proccess Simulation
 
+import random
+import math
+
 def main():
-    print("hey there")
+    # Parâmetros
+    N = 500
+    lamb = 5
+
+    # Listas
+    dt_list = []       # intervalos entre eventos
+    arrival_times = [] # tempos acumulados
+
+    # Tempo inicial
+    current_time = 0 
+
+    # Gerar eventos
+    for i in range(N):
+        u = random.random()  # número uniforme [0,1) Graças a função random usada (meter no relatorio)
+
+        # transformação exponencial
+        dt = -math.log(1 - u) / lamb
+
+        dt_list.append(dt)
+
+        current_time += dt
+        arrival_times.append(current_time)
+
+    # Mostrar primeiros resultados
+    print("Primeiros 10 intervalos dt:")
+    print(dt_list[:10])
+
+    print("\nPrimeiros 10 tempos de chegada:")
+    print(arrival_times[:10])
+
+def hist(): 
+    print("") # here: make the histograms and output them in a excel file
 
 if __name__=="__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Traffic-Engineering-Projects
+
 This reporitory is for the three projects developed in the course of Traffic Engineering in ULisboa - Instituto Superior Tecnico 2025/2026
