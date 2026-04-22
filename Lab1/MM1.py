8988c0ec6a8646a8f49602408c21f759ad25f406
J0s3221 <athydeforking@gmail.com>
2026-04-22 16:26:50 +0100

    feat: changed the structure of the files so each lab project is in in its own folder, also added file for the next step of the first lab

---
+++ Lab1/MM1.py
@@ -0,0 +1,13 @@
+# Etraf first project -- M/M/1 queue simulation
+
+import sys
+import os
+import random
+import math
+import matplotlib.pyplot as plt
+
+def main():
+    print("To Do")
+
+if __name__=="__main__":
+    main()
